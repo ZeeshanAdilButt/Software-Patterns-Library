bab3bf4844aefaaf6a3d39581e0af8e94d8f8ca5
Zeeshan Adil <zeeshanadil56@gmail.com>
2020-08-24 00:34:45 +0500

    implemented fluent validations

--- source/Startup.cs
+++ source/Startup.cs
@@ -1,20 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using CQRS.Context;
+using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace source
 {
@@ -51,7 +45,7 @@ namespace source
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddControllers();
 
-            //services.AddValidatorsFromAssembly(typeof(Startup).Assembly);
+            services.AddValidatorsFromAssembly(typeof(Startup).Assembly);
 
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
---
+++ source/Validators/CreateProductCommndValidator.cs
@@ -0,0 +1,18 @@
+﻿using CQRS.Features.ProductFeatures.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRS.Validators
+{
+    public class CreateProductCommndValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommndValidator()
+        {
+            RuleFor(c => c.Barcode).NotEmpty();
+            RuleFor(c => c.Name).NotEmpty();
+        }
+    }
+}
